d58361d10fbc21ec2592817df2a796d9a8a88ab2
梁海明 <haiming_liang@hotmail.com>
2020-11-14 14:04:03 +0800

    using resilience4j for circuit breaker: Haiming

--- src/main/java/com/haiming/ribboncustomerservice/controller/CustomerController.java
+++ src/main/java/com/haiming/ribboncustomerservice/controller/CustomerController.java
@@ -4,10 +4,15 @@ package com.haiming.ribboncustomerservice.controller;
 import com.haiming.ribboncustomerservice.integration.CoffeeService;
 import com.haiming.ribboncustomerservice.model.Coffee;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerOpenException;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.vavr.control.Try;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.Collections;
 import java.util.List;
@@ -19,11 +24,18 @@ public class CustomerController {
     @Autowired
     private CoffeeService coffeeService;
 
+
+    private CircuitBreaker circuitBreaker;
+
+    public CustomerController(CircuitBreakerRegistry registry){
+        circuitBreaker = registry.circuitBreaker("menu");
+    }
     @GetMapping("/menu")
-    @HystrixCommand(fallbackMethod = "fallbackReadMenu")
     public List<Coffee> readMenu(){
-        List<Coffee> list = coffeeService.getAll();
-        return list;
+        return Try.ofSupplier(CircuitBreaker.decorateSupplier(circuitBreaker,
+                () -> coffeeService.getAll()))
+                .recover(CircuitBreakerOpenException.class, Collections.emptyList())
+                .get();
     }
 
 
